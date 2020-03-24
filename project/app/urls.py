7b4893ac4951774d1eda1c8020190889fd53a907
vincentmuya <vincentmuya13@gmail.com>
2020-03-24 17:48:09 +0300

    added access token

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 urlpatterns=[
     url(r'^$',views.index, name ='index'),
     url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
     url(r'^edit/profile/(\d+)$', views.update_profile, name='update-profile'),
+    url('access/token', views.getAccessToken, name='get_mpesa_access_token'),
 
 ]
 
--- project/app/views.py
+++ project/app/views.py
@@ -4,10 +4,25 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.db import transaction
 from .forms import UserForm,ProfileForm
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 
 # Create your views here.
-@login_required(login_url='/accounts/login')
+def getAccessToken(request):
+    consumer_key = "rC5JYdE2Kf0ivU6C0BHyG0Cp0PGi9rIZ"
+    consumer_secret= "Cb1ZOEx9Qv8VOvXg"
+    api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+    testing = "Hello World"
+    context = {"validated_mpesa_access_token":validated_mpesa_access_token, "testing":testing}
+    # return HttpResponse(validated_mpesa_access_token)
+    return render(request, 'accesstoken.html',context )
+
 def index(request):
     return render(request, 'index.html')
 
